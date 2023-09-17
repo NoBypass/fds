69c1086ae597f1eccf9a943c77b7da8808c474fc
NoBypass <nobypass.yt@gmail.com>
2023-09-17 13:11:08 +0200

    revert some middleware changes

--- server/main.go
+++ server/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/rs/cors"
 	"net/http"
 	"server/src/api/handlers/logger"
 	"server/src/api/resolvers"
 	"server/src/graph/generated"
-	"server/src/middleware"
 	"server/src/utils"
 )
 
 func main() {
 	logger.Log("Starting server", logger.INFO)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	})
 
 	_, ctx, err := utils.ConnectDB()
 	if err != nil {
@@ -21,19 +28,8 @@ func main() {
 		logger.Log("Connected to database", logger.SUCCESS)
 	}
 
-	ws, err := middleware.Auth(ctx, resolvers.WebSocketHandler)
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-	graphql, err := middleware.Auth(ctx, resolvers.GraphQLHandler)
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-
-	http.Handle("/ws", ws)
-	http.Handle("/graphql", graphql)
+	http.Handle("/ws", resolvers.WebSocketHandler(ctx))
+	http.Handle("/graphql", c.Handler(resolvers.GraphQLHandler(ctx)))
 
 	generated.InitSchema()
 	logger.Log("Server started & graphql initialized", logger.SUCCESS)
--- server/src/middleware/authMiddleware.go
+++ server/src/middleware/authMiddleware.go
@@ -1,11 +1,3 @@
 package middleware
 
-import (
-	"context"
-	"net/http"
-)
-
-func Auth(ctx context.Context, next func(context.Context) http.Handler) (http.Handler, error) {
-	// TODO
-	return next(ctx), nil
-}
+// TODO Implement
