3231a8b8a8994a36956b3feb97c71e9d732e8f82
NoBypass <nobypass.yt@gmail.com>
2023-09-17 12:52:10 +0200

    some documentation

--- server/src/repository/db/actions.go
+++ server/src/repository/db/actions.go
@@ -1,9 +1,15 @@
 package db
 
+// Find will find a node in the database by the values
+// of the struct passed in. It will return the node
+// found in the database, or an error if one occurred.
 func (db *DB[T]) Find(entity *T) (*T, error) {
 	return db.common(entity, "MATCH")
 }
 
+// Create will create a node in the database by the struct
+// passed in. It will return the node created in the database,
+// or an error if one occurred.
 func (db *DB[T]) Create(entity *T) (*T, error) {
 	return db.common(entity, "CREATE")
 }
