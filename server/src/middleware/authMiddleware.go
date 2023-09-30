a5da679c6006a239c95428446ef8ed90f5709af4
NoBypass <nobypass.yt@gmail.com>
2023-09-30 15:16:10 +0200

    auth middleware

--- server/src/middleware/authMiddleware.go
+++ server/src/middleware/authMiddleware.go
@@ -1,3 +1,29 @@
 package middleware
 
-// TODO Implement
+import (
+	"context"
+	"net/http"
+	"server/src/auth"
+)
+
+const (
+	SUBSCRIBER = "subscriber"
+	ADMIN      = "admin"
+	USER       = "user"
+	BOT        = "bot"
+)
+
+func Auth(ctx context.Context, run func(ctx context.Context)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx = context.WithValue(ctx, "req", r)
+		ctx = context.WithValue(ctx, "res", w)
+
+		claims, err := auth.ParseJWT(ctx, r.Header.Get("Authorization"))
+		if err == nil {
+			ctx = context.WithValue(ctx, "claims", claims)
+		}
+
+		RateLimiterMiddleware(ctx)
+		run(ctx)
+	})
+}
