638873eb27b3c99a29937e810f888ac6b415a5c4
NoBypass <nobypass.yt@gmail.com>
2023-10-17 17:38:30 +0200

    download page

--- client/src/routes/download/+page.svelte
+++ client/src/routes/download/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
     import Text from '$lib/components/Text.svelte'
+    import Button from '$lib/components/Button.svelte'
+    import Link from '$lib/components/Link.svelte'
 </script>
 
-<Text tw="text-3xl lg:text-5xl text-center">
-    <Text type="s" color="gradient" b>Sessions</Text>, live <Text type="s" color="gradient" b>Tracking</Text>
-    <br>
-    and much more
-</Text>
+<main class="w-full items-center flex flex-col">
+    <Text tw="text-3xl lg:text-5xl text-center">
+        <Text type="s" color="gradient" b>Sessions</Text>, live <Text type="s" color="gradient" b>Tracking</Text>
+        <br>
+        and much more
+    </Text>
+
+    <section class="py-24 text-center grid gap-1.5">
+        <Text color="darkened">100% Open-Source and Safe</Text>
+        <Button type="primary" tw="pb-3 pt-2 px-10" href="https://github.com/NoBypass/fds" rounded>
+            <Text type="h2" b>
+                Download v1.0.0 <!-- TODO: Get version from GH API & Direct download latest stable -->
+            </Text>
+        </Button>
+    </section>
+
+    <section>
+        <Text type="h1" b>Features</Text>
+    </section>
+</main>
