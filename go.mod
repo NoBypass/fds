96e2095ad461dd6f1173b18d801252c92c971065
NoBypass <nobypass.yt@gmail.com>
2024-01-02 02:29:16 +0100

    discord bot authentication

--- go.mod
+++ go.mod
@@ -10,7 +10,9 @@ require (
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/labstack/echo-jwt/v4 v4.2.0 // indirect
 	github.com/labstack/echo/v4 v4.11.3 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
--- internal/core/controller/discord_controller.go
+++ internal/core/controller/discord_controller.go
@@ -2,17 +2,22 @@ package controller
 
 import (
 	"errors"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/surrealdb/surrealdb.go"
 	"net/http"
 	"server/internal/core/custom_err"
 	"server/internal/core/repository"
+	"server/internal/pkg/conf"
+	"server/internal/pkg/consts"
 	"server/internal/pkg/model"
+	"time"
 )
 
 type DiscordController interface {
 	Signup(c echo.Context) error
 	Daily(c echo.Context) error
+	BotLogin(c echo.Context) error
 }
 
 type discordController struct {
@@ -53,4 +58,33 @@ func (r *discordController) Daily(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, *member)
+}
+
+func (r *discordController) BotLogin(c echo.Context) error {
+	input := model.DiscordBotLoginInput{}
+	err := c.Bind(&input)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "invalid request query")
+	}
+
+	if input.Pwd != c.Get("config").(*conf.Config).Authentication.Bot.Password {
+		return c.String(http.StatusForbidden, "invalid password")
+	}
+
+	claims := jwt.RegisteredClaims{
+		Issuer:   consts.JWTCore,
+		Subject:  consts.JWTBot,
+		Audience: []string{consts.JWTBot},
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(c.Get("config").(*conf.Config).Authentication.Jwt.Secret))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": signedToken,
+	})
 }
